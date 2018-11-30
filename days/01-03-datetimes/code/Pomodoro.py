36ee7db5a09815a602a8855d134fd907dca436c6
David Buck <david.x.buck@icloud.com>
2018-11-30 00:46:38 +0000

    Changed naming convention. Added pytest tests.

---
+++ days/01-03-datetimes/code/Pomodoro.py
@@ -0,0 +1,10 @@
+'''
+Pomodoro Timer code There are six steps in the original technique:
+
+Decide on the task to be done.
+Set the pomodoro timer (traditionally to 25 minutes).[1]
+Work on the task.
+End work when the timer rings and put a checkmark on a piece of paper.[5]
+If you have fewer than four checkmarks, take a short break (3–5 minutes), then go to step 2.
+After four pomodoros, take a longer break (15–30 minutes), reset your checkmark count to zero, then go to step 1.
+'''
